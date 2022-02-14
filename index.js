58e2c6cb0b788b70fb2625c6573c63cc8c4ae50d
PanzerWar3 <95978430+PanzerWar3@users.noreply.github.com>
2022-02-14 16:50:43 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBtMG_-_q6WlpoALPmsGvCMGYdC-zOHCc0",
+  authDomain: "catworld-585a6.firebaseapp.com",
+  projectId: "catworld-585a6",
+  storageBucket: "catworld-585a6.appspot.com",
+  messagingSenderId: "171095801245",
+  appId: "1:171095801245:web:eb22106f5f2c119192c374",
+  measurementId: "G-4EVE0PYV8V"
+};
+
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
